1e0f801d8b7db5345d1750830ef9a398a73bd678
satifah <85227592+satifah@users.noreply.github.com>
2021-10-16 17:05:15 +0700

    Add files via upload

---
+++ Tugas4.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas4;
+import UI.Home;
+
+/**
+ *
+ * @author user
+ */
+public class Tugas4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Home fHome = new Home();
+        fHome.setVisible(true);
+    }
+    
+}
